91288160de4ab905f4b20b7e0e86cb1c19d51a4f
lgcavalheiro <lg.cavalheiro@hotmail.com>
2021-03-17 00:11:37 +0000

    class 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,9 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {Course, sortCoursesBySeqNo} from '../model/course';
 import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
 import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import { CoursesService } from '../services/courses.service';
 
 
 @Component({
@@ -19,23 +19,25 @@ export class HomeComponent implements OnInit {
   advancedCourses: Course[];
 
 
-  constructor(private http: HttpClient, private dialog: MatDialog) {
+  constructor(private coursesService: CoursesService, private dialog: MatDialog) {
 
   }
 
   ngOnInit() {
+    const courses$ = this.coursesService.loadAllCourses();
 
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
 
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
+    // this.http.get('/api/courses')
+    //   .subscribe(
+    //     res => {
 
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
+    //       const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
 
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
+    //       this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
 
-        });
+    //       this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
+
+    //     });
 
   }
 
